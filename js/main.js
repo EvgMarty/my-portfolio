03c7cd674def124c8411541035ac976bd4123dc3
Yevhen Martyniuk <132136023+EvgMarty@users.noreply.github.com>
2023-05-20 13:01:01 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const darkModeBtn = document.querySelector('.dark-mode-btn');
+
+//Смотрим что в localStorage Если Дарк то добавляем тему и делаем кнопку активной
+if(localStorage.getItem('DarkMode') === 'dark'){
+   darkModeBtn.classList.add('dark-mode-btn--active');
+   const isDark = document.body.classList.add('dark');
+}
+
+
+//Включение ночного режима по кнопке
+darkModeBtn.onclick = function(){
+   darkModeBtn.classList.toggle('dark-mode-btn--active');
+   const isDark = document.body.classList.toggle('dark');
+
+   if(isDark){
+      localStorage.setItem('DarkMode', 'dark');
+   }else{
+      localStorage.setItem('DarkMode', 'light');
+   }
+}
